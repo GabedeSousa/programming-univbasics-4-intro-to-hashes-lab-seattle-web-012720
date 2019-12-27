9b99de2d9577fc6641f3d3d74c3a1f100dfbaa5f
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-27 23:41:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,11 @@ def id_generator
 end
 
 
-def my_hash_creator(name,)
+def my_hash_creator(name, "Grace Hopper")
   # return a hash that includes the key and value parameters passed into this method
   my_hash_creator = {:name => "Grace Hopper"}
   end
+  
   
   
   
